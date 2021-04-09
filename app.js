1474418c62caadcc813a11066c1e83938fc01070
LiubovKleimenova <kleimenovaliubov@gmail.com>
2021-04-08 20:51:30 -0400

    add back btn, hide human data with private method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const submitBtn = document.getElementById("btn");
-const form = document.getElementById("dino-compare");
-const grid = document.getElementById("grid");
+const form = document.getElementById('dino-compare');
+const grid = document.getElementById('grid');
+const backBtn = document.getElementById('back-btn');
 
 class Being {
 	constructor(species, diet, height, weight) {
@@ -13,7 +13,7 @@ class Being {
 
 class Dino extends Being {
 	constructor(species, diet, height, weight, fact, when, where) {
-		super(species, diet, height * 0.3048, weight * 0.453592); //coefficients are to turn lb to kg, feet to m
+		super(species, diet, height * 0.3048, weight * 0.453592); // coefficients are to turn lb to kg, feet to m
 		this.fact = fact;
 		this.when = when;
 		this.where = where;
@@ -22,7 +22,7 @@ class Dino extends Being {
 
 class Human extends Being {
 	constructor(name, diet, height, weight) {
-		super("human", diet, height, weight);
+		super('human', diet, height, weight);
 		this.name = name;
 	}
 }
@@ -70,62 +70,57 @@ Dino.prototype.getRandomFact = function getRandomFact(human) {
 };
 
 async function getDinosData() {
-	const data = await fetch("/dino.json");
+	const data = await fetch('/dino.json');
 	const dinoData = await data.json();
 	return dinoData.Dinos;
 }
 
-// function getHumanData() {
-// 	//make IIFE instead
-// 	return {
-// 		name: document.getElementById("name").value,
-// 		diet: document.getElementById("diet").value,
-// 		weight: Number(document.getElementById("weight").value),
-// 		height:
-// 			Number(document.getElementById("meters").value) +
-// 			0.01 * Number(document.getElementById("cm").value),
-// 	};
-// }
-
 function toggleScreen() {
-	form.classList.add("hide");
-	grid.classList.remove("hide");
+	form.classList.toggle('hide');
+	grid.classList.toggle('hide');
+	backBtn.classList.toggle('hide');
 }
 
 function generateTile(animal, human) {
-	let tile = document.createElement("div");
-	let name = document.createElement("h3");
-	let image = document.createElement("img");
-	let fact = document.createElement("p");
+	let tile = document.createElement('div');
+	let name = document.createElement('h3');
+	let image = document.createElement('img');
+	let fact = document.createElement('p');
 
 	name.innerHTML = animal instanceof Dino ? animal.species : animal.name; // name for human, species for other
 	fact.innerHTML =
-		animal instanceof Dino && animal.species !== "Pigeon"
+		animal instanceof Dino && animal.species !== 'Pigeon'
 			? animal.getRandomFact(human)
 			: animal.fact; // random for dinos, single for pigeon
-	image.setAttribute("src", `/images/${animal.species}.png`);
-	image.setAttribute("alt", animal.species);
+	image.setAttribute('src', `/images/${animal.species}.png`);
+	image.setAttribute('alt', animal.species);
 
-	tile.classList.add("grid-item");
+	tile.classList.add('grid-item');
 	tile.appendChild(name);
 	tile.appendChild(image);
 	if (animal instanceof Dino) {
-		tile.appendChild(fact); // do nor append fact to human tile
+		tile.appendChild(fact); // do not append fact to human tile
 	}
 	grid.appendChild(tile);
 }
 
-form.addEventListener("submit", function (e) {
-	// Use IIFE to get human data from form (??) why??
+form.addEventListener('submit', function (e) {
 	e.preventDefault();
-	let humanData = (function() {
+
+	// use IIFE to make human data ptivate
+	let humanData = (function () {
+		let name = document.getElementById('name').value;
+		let diet = document.getElementById('diet').value;
+		let weight = Number(document.getElementById('weight').value);
+		let height =
+			Number(document.getElementById('meters').value) +
+			0.01 * Number(document.getElementById('cm').value);
+
 		return {
-			name: document.getElementById("name").value,
-			diet: document.getElementById("diet").value,
-			weight: Number(document.getElementById("weight").value),
-			height:
-				Number(document.getElementById("meters").value) +
-				0.01 * Number(document.getElementById("cm").value),
+			name: name,
+			diet: diet,
+			weight: weight,
+			height: height,
 		};
 	})();
 
@@ -155,4 +150,9 @@ form.addEventListener("submit", function (e) {
 	});
 
 	toggleScreen();
+});
+
+backBtn.addEventListener('click', function () {
+	toggleScreen();
+	grid.innerHTML = '';
 });
